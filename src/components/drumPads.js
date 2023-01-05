68c22228b371dfe8fc1828af777f31d723bb891c
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-05 09:44:53 +0530

    initial setup is ready

---
+++ src/components/drumPads.js
@@ -0,0 +1,47 @@
+export const drumpads = [
+    {
+        keyCode: 81,
+        text: "Q",
+        sec: "",
+    },
+    {
+        keyCode: 87,
+        text: "W",
+        sec: "",
+    },
+    {
+        keyCode: 69,
+        text: "E",
+        sec: "",
+    },
+    {
+        keyCode: 65,
+        text: "A",
+        sec: "",
+    },
+    {
+        keyCode: 83,
+        text: "S",
+        sec: "",
+    },
+    {
+        keyCode: 68,
+        text: "D",
+        sec: "",
+    },
+    {
+        keyCode: 90,
+        text: "Z",
+        sec: "",
+    },
+    {
+        keyCode: 88,
+        text: "X",
+        sec: "",
+    },
+    {
+        keyCode: 67,
+        text: "C",
+        sec: "",
+    },
+]
